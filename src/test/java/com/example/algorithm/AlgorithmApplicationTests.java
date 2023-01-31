10619e798209b2b2ed4c0f17342dbaff16b87e3c
JeonGuKang <jgkang@modoric.com>
2023-01-31 23:57:16 +0900

    feat : 알고리즘 문제 풀이 정수 내림차순으로 배치하기 https://school.programmers.co.kr/learn/courses/30/lessons/12933

--- src/test/java/com/example/algorithm/AlgorithmApplicationTests.java
+++ src/test/java/com/example/algorithm/AlgorithmApplicationTests.java
@@ -2,10 +2,26 @@ package com.example.algorithm;
 
 import com.example.algorithm.linked_list.SingleLinkedList;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 
 class AlgorithmApplicationTests {
+
+    @Test
+    void 내림차순() {
+        long value = 118372L;
+        final int[] ints = Long.toString(value).chars().map(x -> x - '0').toArray();
+        String result = Arrays.stream(Arrays.stream(ints).boxed()
+                .sorted(Collections.reverseOrder())
+                .mapToInt(Integer::intValue)
+                .toArray())
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(""));
+        System.out.println(result);
+    }
 
     @Test
     void contextLoads() {
