6953b426e252a64b5700061feece61ff01e1c266
Jeongukang <amper2421@naver.com>
2023-11-19 22:21:57 +0900

    feat : dfs 길찾기 알고리즘 추가

---
+++ src/test/java/com/example/algorithm/maze/dfs/DfsMazeTest.kt
@@ -0,0 +1,60 @@
+package com.example.algorithm.maze.dfs
+
+import org.junit.jupiter.api.Test
+
+class DfsMazeTest {
+
+    val maze = arrayOf(
+        intArrayOf(1, 1, 0, 0, 0),
+        intArrayOf(0, 1, 1, 1, 1),
+        intArrayOf(0, 0, 0, 0, 1),
+        intArrayOf(1, 1, 1, 1, 1),
+        intArrayOf(1, 1, 1, 1, 1)
+    )
+
+    val rows = 5
+    val cols = 5
+
+    val visited = Array(rows) { BooleanArray(cols) }
+
+    val startX = 0
+    val startY = 0
+    val endX = 4
+    val endY = 4
+
+    val dx = intArrayOf(-1, 1, 0, 0)
+    val dy = intArrayOf(0, 0, -1, 1)
+
+    fun isValid(x: Int, y: Int): Boolean {
+        return x in 0 until rows && y in 0 until cols && maze[x][y] == 1 && !visited[x][y]
+    }
+
+    fun dfs(x: Int, y: Int): Boolean {
+        if (x == endX && y == endY) return true
+
+        visited[x][y] = true
+
+        for (i in 0 until 4) {
+            val newX = x + dx[i]
+            val newY = y + dy[i]
+
+            if (isValid(newX, newY)) {
+                if (dfs(newX, newY)) {
+                    return true
+                }
+            }
+        }
+
+        return false
+    }
+
+    @Test
+    fun main() {
+        if (dfs(startX, startY)) {
+            println("경로가 존재합니다.")
+        } else {
+            println("경로가 존재하지 않습니다.")
+        }
+    }
+
+}
